25ed9fb2c58c598f317776c97e3a3c146f68fb06
Steeve Matou <steeve.matou@gmail.com>
2023-08-21 19:06:57 +0200

    :tada: feat/ adapte a middleware to validate jwt token

--- app/router/auth.router.ts
+++ app/router/auth.router.ts
@@ -1,10 +1,26 @@
 import express, { Router } from 'express';
+import authController from '../controllers/auth.controllers.ts';
+import factory from '../middleware/factory.controller.ts';
+import validateToken from '../middleware/validate.token.ts';
 
 const router: Router = express.Router();
 
-router.route('/signup').post();
-router.route('/signin').post();
-router.route('user/validate').get();
-router.route('/logout').post();
+const {
+	register, signin, validate, logout,
+} = authController;
+
+// TODO add validation middleware
+
+router.route('/signup')
+	.post(factory(register));
+
+router.route('/signin')
+	.post(factory(signin));
+
+router.route('user/validate')
+	.get(validateToken, factory(validate));
+
+router.route('/logout')
+	.post(logout);
 
 export default router;
