0e2b46ae9bae79d379633ba08de5d8dddcf47683
Steeve Matou <steeve.matou@gmail.com>
2023-08-22 18:35:00 +0200

    :tada: feat/add validation on auth route

--- app/@types/index.d.ts
+++ app/@types/index.d.ts
@@ -2,14 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 
 export type Controller = (req: Request, res: Response, next?: NextFunction) => any | Promise<any>;
 
-export const canals = {
-  body: 'body',
-  params: 'params',
-  query: 'query',
-} as const;
-
 export type LoginForm = {
-  emailOrUsername: string;
+  username: string;
   password: string;
 };
 
