7002fbfeb9d6aef6f0f8d44802d4edcb290671f7
Steeve Matou <steeve.matou@gmail.com>
2023-08-22 21:10:01 +0200

    :construction: update function still in progress

--- app/models/user_on_friend.ts
+++ app/models/user_on_friend.ts
@@ -1,17 +1,64 @@
 import prisma from '../helpers/db.client.ts';
+import type { FriendRequestStatus } from '../@types/index.js';
 
 export default {
   // get all friends of an user
+  // pending and accepted
   findByUserId: async (userId: number) => {
     const result = await prisma.user_on_friend.findMany({
       where: {
-        userId,
+        asker_id: userId,
+        status: 'accepted' || 'pending',
       },
       include: {
-        friend: true,
+        asked: true,
+        asker: true,
       },
     });
     await prisma.$disconnect();
     return result;
+  },
+  // get all pending friend requests of an user
+  // this is requests user have to accept or reject
+  findByFriendId: async (userId: number) => {
+    const result = await prisma.user_on_friend.findMany({
+      where: {
+        asked_id: userId,
+        status: 'pending',
+      },
+      include: {
+        asker: true,
+        asked: true,
+      },
+    });
+    await prisma.$disconnect();
+  },
+  // create a pending friend request on the 2 sides of the relationship
+  create: async (askerId: number, askedId: number) => {
+    const result = await prisma.user_on_friend.createMany({
+      data: [{
+        asker_id: askerId,
+        asked_id: askedId,
+        status: 'pending',
+      }, {
+        asker_id: askedId,
+        asked_id: askerId,
+        status: 'pending',
+      }],
+    });
+    await prisma.$disconnect();
+    return result;
+  },
+  // this is not working
+  update: async (askerId: number, askedId: number, status: FriendRequestStatus) => {
+    const result = await prisma.user_on_friend.update({
+      where: {
+        asker: askerId,
+        asked: askedId,
+      },
+      data: {
+        status,
+      },
+    });
   },
 };
