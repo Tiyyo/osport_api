022f8608661f36adfce23e6315eae7e2caa1bdc1
Steeve Matou <111092370+Tiyyo@users.noreply.github.com>
2023-08-25 00:30:33 +0200

    :hammer: restore factory controller

--- app/middleware/factory.controller.ts
+++ app/middleware/factory.controller.ts
@@ -1,25 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
-import { Redis } from 'ioredis';
-import logger from '../helpers/logger.ts';
-
-const redis = new Redis();
+import type { Controller } from '../@types/index.d.ts';
 
 // factory design pattern
 // it returns a middleware function
 
-export default (key: string) => async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const cacheValue = await redis.get(key).catch((error) => {
-    logger.error(error);
-    next();
+export default (controller: Controller) => (
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await controller(req, res, next);
+    } catch (err) {
+      next(err);
+    }
   });
-  if (cacheValue) {
-    // not modfied
-    res.status(304).send(JSON.parse(cacheValue));
-  }
-  req.body.cacheKey = key;
-  next();
-};
+
