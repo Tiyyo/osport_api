f743aa6f9abb9700469b5a5adc916a0708682170
Steeve Matou <steeve.matou@gmail.com>
2023-08-24 18:38:58 +0200

    :zap: set of utility function to cache data

--- app/middleware/cache.ts
+++ app/middleware/cache.ts
@@ -1,12 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
+import logger from '../helpers/logger.ts';
+import Cache from '../service/cache.ts';
 
-async function cacheMiddleware(
+export default (key: string) => async (
   req: Request,
   res: Response,
   next: NextFunction,
-  key: string,
-) {
-  next();
-}
-
-export default cacheMiddleware;
+) => {
+  if (!key) next();
+  let paramsKey = key;
+  if (req.params.id) paramsKey = key + req.params.id;
+  try {
+    const cacheValue = await Cache.get(paramsKey);
+    if (req.method === 'GET' && cacheValue) {
+      // not modfied
+      res.status(304).send(cacheValue);
+    }
+    req.body.cacheKey = paramsKey;
+    next();
+  } catch (error) {
+    logger.error(error);
+    next();
+  }
+};
