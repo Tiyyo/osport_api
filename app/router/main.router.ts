39c86d5e6204dcf2c3c8bc160b4a36d40e5d04c0
Steeve Matou <steeve.matou@gmail.com>
2023-08-25 09:21:51 +0200

    :tada: events participants complete

--- app/router/main.router.ts
+++ app/router/main.router.ts
@@ -3,12 +3,15 @@ import errorHandler from '../middleware/error.handler.ts';
 import authRouter from './auth.router.ts';
 import userRouter from './user.router.ts';
 import NotFoundError from '../helpers/errors/notFound.error.ts';
+import participantRouter from './participants.router.ts';
 
 const router: Router = express.Router();
 
 router.use('/', authRouter);
 
 router.use('/user', userRouter);
+
+router.use('/participant/event', participantRouter);
 
 router.get('/test', (_req, res) => {
   res.status(200).send({
---
+++ app/router/participants.router.ts
@@ -0,0 +1,28 @@
+import express, { Router } from 'express';
+import participantController from '../controllers/participants.controller.ts';
+import factory from '../middleware/factory.controller.ts';
+// import validateSchema from '../middleware/schemas.validator.ts';
+// import canals from '../helpers/canals.ts';
+
+const router: Router = express.Router();
+
+// All methods listed in the controller
+const {
+  getParticipants, sendInivation, updateStatus,
+} = participantController;
+
+// we need to ensure that the data is valid before reaching the controller
+// we use the same middleware throughout the app
+// canals represent which part of the request we want to validate
+
+// get all participants for an event
+router.route('/:id')
+  .get(factory(getParticipants));
+
+// sent invivation to user
+router.route('/')
+  .post(factory(sendInivation))
+  // accept or decline invitation
+  .patch(factory(updateStatus));
+
+export default router;
