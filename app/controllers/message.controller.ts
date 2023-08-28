4804cc2543a95cc6b8a07897f1cd59ac41af023e
Steeve Matou <111092370+Tiyyo@users.noreply.github.com>
2023-08-28 12:19:30 +0200

    :hammer:

--- app/controllers/message.controller.ts
+++ app/controllers/message.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Message from '../models/message.ts';
 import checkParams from '../utils/checkParams.ts';
+// import Cache from '../service/cache.ts';
 
 export default {
   getHistoric: async (req: Request, res: Response) => {
@@ -9,49 +10,48 @@ export default {
 
     const historic = await Message.findMany(event_id);
 
+    // await Cache.set(`chat${event_id}`, historic);
+
     res.status(200).json({ message: 'Historic retrieved successfully', data: historic });
   },
   create: async (req: Request, res: Response) => {
     const { event_id, user_id, message } = req.body;
 
-    console.log(event_id, user_id, message);
+    // await Cache.del([`chat${event_id}`]);
 
-    // just for testing
-    const event_id_testing = checkParams(event_id);
-    const user_id_testing = checkParams(user_id);
-
-    // when model is ready, we will
-    // check if user is in the list of participants
-
-    await Message.create({ event_id: event_id_testing, user_id: user_id_testing, message });
+    await Message.create({ event_id, user_id, message });
 
     res.status(201).json({ message: 'Message created successfully' });
   },
   update: async (req: Request, res: Response) => {
-    const { id, message } = req.body;
+    const { id, eventId, message } = req.body;
 
-    // just for testing
-    const checkedId = checkParams(id);
+    // await Cache.del([`chat${eventId}`]);
 
-    await Message.update(checkedId, message);
+    await Message.update(id, message);
 
     res.status(204).json({ message: 'Message updated successfully' });
   },
   destroyOne: async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const checkedId = checkParams(id);
+    const id = checkParams(req.params.id);
 
-    await Message.destroyOne(checkedId);
+    const message = await Message.findOne(id);
+
+    if (!message) return;
+
+    // await Cache.del([`chat${message.event_id}`]);
+
+    await Message.destroyOne(id);
 
     res.status(204).json({ message: 'Message deleted successfully' });
   },
   destroyMany: async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const event_id = checkParams(id);
+    const event_id = checkParams(req.params.id);
+
+    // await Cache.del([`chat${event_id}`]);
 
     await Message.destroyMany(event_id);
 
     res.status(204).json({ message: 'Historic deleted successfully' });
   },
-
 };
