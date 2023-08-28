ff29a519e61bcd614c3e2c36ffd2b01017e5f87b
Steeve Matou <111092370+Tiyyo@users.noreply.github.com>
2023-08-28 12:18:24 +0200

    :hammer:

--- app/controllers/friendlist.controller.ts
+++ app/controllers/friendlist.controller.ts
@@ -1,14 +1,11 @@
 import { Request, Response } from 'express';
 import Friends from '../models/user_on_friend.ts';
 import NotFoundError from '../helpers/errors/notFound.error.ts';
+import checkParams from '../utils/checkParams.ts';
 
 export default {
-  // get all friends of a user with pending or accepted status
   getPendingRequestSent: async (req: Request, res: Response) => {
-    // get the user id from the request
-    // get the friends of the user
-    // return the friends
-    const userId = req.params.id; // this is a string
+    const userId = checkParams(req.params.id);
     const status = 'pending';
 
     const friends = await Friends.find(Number(userId), status);
@@ -16,11 +13,7 @@ export default {
     res.status(200).json({ message: 'Friends retrieved successfully', data: friends });
   },
   getAcceptedFriends: async (req: Request, res: Response) => {
-    // get the user id from the request
-    // get the friends of the user
-    // return the friends
-    //  ask frontend for an array of students
-    const userId = req.params.id; // this is a string
+    const userId = checkParams(req.params.id);
     const status = 'accepted';
 
     const friends = await Friends.find(Number(userId), status);
@@ -28,57 +21,50 @@ export default {
     res.status(200).json({ message: 'Friends retrieved successfully', data: friends });
   },
   getPendingRequestReceived: async (req: Request, res: Response) => {
-    // get the user id from the request
-    // return the friends request to accept or reject
-    const userId = req.params.id; // this is a string
+    const userId = checkParams(req.params.id);
 
-    const friends = await Friends.findManyRequest(Number(userId));
+    const friends = await Friends.findManyRequest(userId);
 
     res.status(200).json({ message: 'Friends retrieved successfully', data: friends });
   },
-  // send a friend request to a user
   sendFriendRequest: async (req: Request, res: Response) => {
-    // askerId is the user who sends the request so userId
-    // userid
-    // userIdToAddFriend
     const data = {
-      askerId: Number(req.body.userId),
-      askedId: Number(req.body.askedId),
+      asker_id: Number(req.body.userId),
+      asked_id: Number(req.body.friendId),
     };
 
     await Friends.create(data);
 
     res.status(201).json({ message: 'Friend request sent successfully' });
   },
-  // accept a friend request
   acceptFriendRequest: async (req: Request, res: Response) => {
-    const { userId, userToAddId } = req.body;
+    const { userId, friendId } = req.body;
 
-    // before updating the friend we need to check if there is a pending request
-    // if there is no pending request we can't accept the friend request
-
-    const isPendingRequestExist = await Friends.findRequest(Number(userId), Number(userToAddId));
-
-    if (!isPendingRequestExist) throw new NotFoundError('No pending friend request found');
+    try {
+      await Friends.findRequest(Number(friendId), Number(userId));
+    } catch (error) {
+      if (error instanceof NotFoundError) return res.status(200).json({ message: 'No pending friend request found' });
+    }
 
     const updateStatus = 'accepted';
     await Friends
-      .update(Number(userId), Number(userToAddId), updateStatus);
+      .update(Number(userId), Number(friendId), updateStatus);
 
-    res.status(204).json({ message: 'Friend request accepted successfully' });
+    return res.status(204).json({ message: 'Friend request accepted successfully' });
   },
-  // reject a friend request
   rejectFriendRequest: async (req: Request, res: Response) => {
-    const { userId, userToAddId } = req.body;
+    const { userId, friendId } = req.body;
 
-    const isPendingRequestExist = await Friends.findRequest(Number(userId), Number(userToAddId));
-
-    if (!isPendingRequestExist) throw new NotFoundError('No pending friend request found');
+    try {
+      await Friends.findRequest(Number(friendId), Number(userId));
+    } catch (error) {
+      if (error instanceof NotFoundError) return res.status(200).json({ message: 'No pending friend request found' });
+    }
 
     const updateStatus = 'rejected';
 
-    await Friends.update(Number(userId), Number(userToAddId), updateStatus);
+    await Friends.update(Number(userId), Number(friendId), updateStatus);
 
-    res.status(204).json({ message: 'Friend request rejected successfully' });
+    return res.status(204).json({ message: 'Friend request rejected successfully' });
   },
 };
