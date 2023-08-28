bf50c9319a7f5d94c29f7f7d789a88470f5e1210
Steeve Matou <111092370+Tiyyo@users.noreply.github.com>
2023-08-28 12:24:39 +0200

    :hammer:

---
+++ app/models/image.ts
@@ -0,0 +1,65 @@
+import prisma from '../helpers/db.client.ts';
+import DatabaseError from '../helpers/errors/database.error.ts';
+
+export default {
+  create: async ({ url, title }: { url: string, title: string }) => {
+    try {
+      const result = await prisma.image.create({
+        data: {
+          url,
+          title,
+        },
+      });
+      await prisma.$disconnect();
+      return result;
+    } catch (error: any) {
+      throw new DatabaseError(error.message, 'image', error);
+    }
+  },
+  findOne: async (args: string) => {
+    try {
+      const result = await prisma.image.findFirst({
+        where: {
+          OR: [
+            { url: args },
+            { title: args },
+          ],
+        },
+      });
+      await prisma.$disconnect();
+      return result;
+    } catch (error: any) {
+      throw new DatabaseError(error.message, 'image', error);
+    }
+  },
+  findByPk: async (id: number) => {
+    try {
+      const result = await prisma.image.findFirst({
+        where: {
+          id,
+        },
+      });
+      await prisma.$disconnect();
+      return result;
+    } catch (error: any) {
+      throw new DatabaseError(error.message, 'image', error);
+    }
+  },
+  update: async (id: number, url: string, title: string) => {
+    try {
+      const result = await prisma.image.update({
+        where: {
+          id,
+        },
+        data: {
+          url,
+          title,
+        },
+      });
+      await prisma.$disconnect();
+      return result;
+    } catch (error: any) {
+      throw new DatabaseError(error.message, 'image', error);
+    }
+  },
+};
