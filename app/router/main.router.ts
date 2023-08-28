3b10b3c64cc1500d7768489b82b9fb00b47e9ec5
Steeve Matou <steeve.matou@gmail.com>
2023-08-28 22:24:06 +0200

    :tada: link algo to database

--- app/router/main.router.ts
+++ app/router/main.router.ts
@@ -7,7 +7,8 @@ import NotFoundError from '../helpers/errors/notFound.error.ts';
 import participantRouter from './participant.router.ts';
 import friendlistRouter from './friendlist.router.ts';
 import messageRouter from './message.router.ts';
-import ratingController from '../controllers/rating.controller.ts';
+import ratingRouter from './rating.router.ts';
+import { generateBalancedTeam } from '../service/generateTeam.ts';
 
 const router: Router = express.Router();
 
@@ -17,9 +18,10 @@ router.use('/chat', messageRouter);
 router.use('/participant/event', participantRouter);
 router.use('/user_friends', friendlistRouter);
 // router.use('/event', eventRouter);
-router.use('/', ratingController);
+router.use('/', ratingRouter);
 
 router.use('/test', async (_req, res) => {
+  generateBalancedTeam(1);
   res.send('test');
 });
 
