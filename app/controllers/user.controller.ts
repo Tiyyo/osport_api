68d307914f4e509219a1a2f5e390e27033da26ba
Steeve Matou <steeve.matou@gmail.com>
2023-08-30 18:25:31 +0200

    :hammer: not sending 500 anymore if image could not be deleted

--- app/controllers/user.controller.ts
+++ app/controllers/user.controller.ts
@@ -5,6 +5,7 @@ import UserOnSport from '../models/user_on_sport.ts';
 // import type { AllowedUserUpdate } from '../@types/index.d.ts';
 import { deleteImageFromServer, saveImageOnServer } from '../service/image.ts';
 import checkParams from '../utils/checkParams.ts';
+import logger from '../helpers/logger.ts';
 
 // To block user who want to modify his own id, createdAt, updatedAt, or image_id
 // const isAllowedUserUpdate = (data: any): data is AllowedUserUpdate => (
@@ -27,6 +28,9 @@ export default {
   updateImage: async (req: Request, res: Response) => {
     const { id } = req.body;
     if (!req.file) return res.status(200).json({ error: 'No image provided' });
+    const error: any = {
+
+    };
 
     const image = req.file.buffer;
 
@@ -42,12 +46,17 @@ export default {
 
     if (user.image_url) {
       // const imageToDelete = await Image.findOne(user.image_url as string);
-      await deleteImageFromServer(user.image_url as string);
+      try {
+        await deleteImageFromServer(user.image_url as string);
+      } catch (err: any) {
+        logger.error(err);
+        error.image = 'Error while deleting image from server';
+      }
     }
 
     const isUpdated = await UserModel.updateUser(id, { imageUrl: imageStored.url });
 
-    return res.status(200).json({ message: 'User has been updated', data: isUpdated });
+    return res.status(200).json({ message: 'User has been updated', data: isUpdated, error });
   },
 
   deleteUser: async (req: Request, res: Response) => {
