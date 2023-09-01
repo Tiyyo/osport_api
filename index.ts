7c9bf1dd0b9fe8f487dfb6b70b561d0fb3354185
Steeve Matou <steeve.matou@gmail.com>
2023-09-01 12:38:23 +0200

    :hammer: remove https code

--- index.ts
+++ index.ts
@@ -8,16 +8,7 @@ import app from './app/index.app.js';
 const PORT = process.env.PORT || 8080;
 
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer({
-//   // key: fs.readFileSync('key.pem'),
-//   // cert: fs.readFileSync('cert.pem'),
-// }, app);
-// const server = createServer(app);
 
 httpServer.listen(PORT, () => {
   logger.info(`HTTP Server is running on port ${PORT}`);
 });
-
-// httpsServer.listen(8443, () => {
-//   logger.info(`HTTPS Server is running on port ${8443}`);
-// });
