065874e08d41989282add8ad76b7bd4961110643
Steeve Matou <steeve.matou@gmail.com>
2023-09-01 18:13:36 +0200

    :zap: test action

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { UserConfig } from 'vitest';
 
 const config: UserConfig | any = {
-  testMatch: ['**/__tests__/**/*.(ts|tsx|js|jsx)'],
-  extensions: ['ts', 'tsx', 'js', 'jsx'],
+  // testMatch: ['**/__tests__/**/*.(ts|tsx|js|jsx)'],
+  // extensions: ['ts', 'tsx', 'js', 'jsx'],
+  exclude: ['./build/**/*', './node_modules/**/*'],
 };
 
 export default config;
