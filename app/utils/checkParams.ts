2556886a6455d41005ffac640a581ea88b12cfb1
Steeve Matou <steeve.matou@gmail.com>
2023-09-03 20:00:19 +0200

    test params

--- app/utils/checkParams.ts
+++ app/utils/checkParams.ts
@@ -4,7 +4,7 @@ function checkParams(params: any) {
   const parsedParams = parseInt(params, 10);
 
   if (Number.isNaN(parsedParams)) {
-    throw new Error('Invalid params');
+    throw new Error('Invalid params : params must be a number');
   }
   return parsedParams;
 }
