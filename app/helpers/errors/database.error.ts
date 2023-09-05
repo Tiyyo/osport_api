c722103aed9377d8afcd364a254a4c07b9b1eb84
Steeve Matou <steeve.matou@gmail.com>
2023-09-05 17:13:56 +0200

    :hammer: del unused directive comments

--- app/helpers/errors/database.error.ts
+++ app/helpers/errors/database.error.ts
@@ -1,6 +1,4 @@
 // ignore all ts errors in this file
-// FIXME remove this once refactor is done
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
 import { Prisma } from '@prisma/client';
