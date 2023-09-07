3f41e1867406c26d66ee343fb8201b11960c2e57
Steeve Matou <steeve.matou@gmail.com>
2023-09-07 21:17:40 +0200

    :zap: add more unit test on middleware

---
+++ app/tests/middleware/validate.token.test.ts
@@ -0,0 +1,48 @@
+import {
+  describe, vi, afterEach, test, expect,
+} from 'vitest';
+import { Request, Response, NextFunction } from 'express';
+import validateToken from '../../middleware/validate.token.js';
+import createToken from '../../helpers/token/create.access.js';
+import '../../helpers/env.load.js';
+
+describe('validate token middleware', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  // @ts-ignore
+  const next = vi.fn() as NextFunction;
+  test('should throw an error if token is not valid', async () => {
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: 'invalid token',
+      },
+    } as Request;
+    validateToken(mockRequest, {} as Response, next);
+    expect(next).toBeCalledWith(expect.any(Error));
+  });
+  test('should call next if token is valid', async () => {
+    const token = createToken('8h', { userId: 1 });
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: token,
+      },
+    } as Request;
+    validateToken(mockRequest, {} as Response, next);
+    expect(next).toBeCalled();
+  });
+  test('should have a decoded property in request body', async () => {
+    const token = createToken('8h', { userId: 1 });
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: token,
+      },
+    } as Request;
+    validateToken(mockRequest, {} as Response, next);
+    expect(mockRequest.body.decoded[0]).toEqual({ userId: 1 });
+  });
+});
---
+++ app/tests/middleware/validate.user.test.ts
@@ -0,0 +1,181 @@
+import {
+  describe, vi, afterEach, test, expect,
+} from 'vitest';
+import { Request, Response, NextFunction } from 'express';
+import validateUser from '../../middleware/validate.user.js';
+import createToken from '../../helpers/token/create.access.js';
+import '../../helpers/env.load.js';
+
+describe('validate user middleware', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  const mockResponse = () => {
+    const res = {} as Response;
+    res.status = vi.fn().mockReturnValue(res);
+    res.json = vi.fn().mockReturnValue(res);
+    return res;
+  };
+
+  // @ts-ignore
+  const next = vi.fn() as NextFunction;
+
+  const token = createToken('8h', { userId: 1 });
+
+  test('should return a status 401 if token is not valid', async () => {
+    const mockRequest = {
+      body: {},
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.status).toBeCalledWith(401);
+  });
+  test('should return a status 401 if token is not valid', async () => {
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: 'invalid token',
+      },
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.status).toBeCalledWith(401);
+  });
+  test('should return a json with an error message if token is not valid', async () => {
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: 'invalid token',
+      },
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.json).toBeCalledWith({ error: 'Unauthorized user' });
+  });
+  test('should retrun a json with an if token doesn\'t exist', async () => {
+    const mockRequest = {
+      body: {},
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.json).toBeCalledWith({ error: 'Unauthorized user' });
+  });
+  test('should return an error if no userId is provided', async () => {
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: token,
+      },
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.json).toBeCalledWith({ error: 'Unauthorized user' });
+  });
+  test('should return an if userId in token is different from userId in params', async () => {
+    // @ts-ignore
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: token,
+      },
+      params: {
+        id: 2,
+      },
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.json).toBeCalledWith({ error: 'Unauthorized user' });
+  });
+  test('should return an json with an error if userId in token is different from userId in body', async () => {
+    const mockRequest = {
+      body: {
+        userId: 2,
+      },
+      cookies: {
+        accessToken: token,
+      },
+      params: {},
+      method: 'PUT',
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.json).toBeCalledWith({ error: 'Unauthorized user' });
+  });
+  test('should retrun an error if userId in params and method is not GET or DELETE', async () => {
+    // @ts-ignore
+    const mockRequest = {
+      body: {
+      },
+      cookies: {
+        accessToken: token,
+      },
+      params: {
+        id: 2,
+      },
+      method: 'PUT',
+    } as Request;
+    const res = mockResponse();
+    await validateUser(mockRequest, res, next);
+    expect(res.json).toBeCalledWith({ error: 'Unauthorized user' });
+  });
+  test('should call next if userId in params and method is GET', async () => {
+    // @ts-ignore
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: token,
+      },
+      params: {
+        id: 1,
+      },
+      method: 'GET',
+    } as Request;
+    await validateUser(mockRequest, mockResponse(), next);
+    expect(next).toBeCalled();
+  });
+  test('should call next if userId in params and method is DELETE', async () => {
+    // @ts-ignore
+    const mockRequest = {
+      body: {},
+      cookies: {
+        accessToken: token,
+      },
+      params: {
+        id: 1,
+      },
+      method: 'DELETE',
+    } as Request;
+    await validateUser(mockRequest, mockResponse(), next);
+    expect(next).toBeCalled();
+  });
+  test('should call next if userId in body and method is PUT', async () => {
+    // @ts-ignore
+    const mockRequest = {
+      body: {
+        userId: 1,
+      },
+      cookies: {
+        accessToken: token,
+      },
+      params: {},
+      method: 'PUT',
+    } as Request;
+    await validateUser(mockRequest, mockResponse(), next);
+    expect(next).toBeCalled();
+  });
+  test('should call next if userId in body and method is PATCH', async () => {
+    const mockRequest = {
+      body: {
+        userId: 1,
+      },
+      cookies: {
+        accessToken: token,
+      },
+      params: {},
+      method: 'PATCH',
+    } as Request;
+    await validateUser(mockRequest, mockResponse(), next);
+    expect(next).toBeCalled();
+  });
+});
