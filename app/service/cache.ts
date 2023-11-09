767ee5fa1a4e81e73f8773a4d3d55320285c4d29
Steeve Matou <steeve.matou@gmail.com>
2023-11-09 03:18:59 +0100

    cleaning build and cache instance

--- app/service/cache.ts
+++ app/service/cache.ts
@@ -1,20 +1,20 @@
-import { Redis } from 'ioredis';
+// import { Redis } from 'ioredis';
 
-const redis = new Redis();
+// const redis = new Redis();
 
-export default class Cache {
-  static DEFAULT_EXPIRATION = 300; // 5 minutes
+// export default class Cache {
+//   static DEFAULT_EXPIRATION = 300; // 5 minutes
 
-  static async get(key: string) {
-    const cacheValue = await redis.get(key);
-    return cacheValue ? JSON.parse(cacheValue) : null;
-  }
+//   static async get(key: string) {
+//     const cacheValue = await redis.get(key);
+//     return cacheValue ? JSON.parse(cacheValue) : null;
+//   }
 
-  static async set(key: string, data: any, expiration: number = Cache.DEFAULT_EXPIRATION) {
-    redis.set(key, JSON.stringify(data), 'EX', expiration);
-  }
+//   static async set(key: string, data: any, expiration: number = Cache.DEFAULT_EXPIRATION) {
+//     redis.set(key, JSON.stringify(data), 'EX', expiration);
+//   }
 
-  static async del(keys: string[]) {
-    await redis.del(...keys);
-  }
-}
+//   static async del(keys: string[]) {
+//     await redis.del(...keys);
+//   }
+// }
